4c775623aef606d3d22582151a94c066cf8a5fe1
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-07-01 21:17:42 +0900

    add icon animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import Loganimation from "./icon-animation.json";
+import { Player } from "@lottiefiles/react-lottie-player";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="mochi-icon-ani">
+          <Player
+            autoplay
+            loop
+            src={Loganimation}
+            style={{ height: "900px", width: "900px" }}
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+          </Player>
+        </div>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
